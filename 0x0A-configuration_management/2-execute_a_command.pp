67de47d626f594872d21268f8af0257b0bc4b680
Calebkillom <Calebkillom@gmail.com>
2023-04-24 20:30:16 +0000

    task 0,1,2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# create a manifest that kills a process named killmenow
+
+exec { 'killmenow':
+  command => 'pkill killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep killmenow',
+}
+
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+iConfiguration management
