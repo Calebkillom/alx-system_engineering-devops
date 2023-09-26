b92a11f73587ad7c7ea480e17c623986ccc936c9
Calebbkillom <Calebbkillom@gmail.com>
2023-09-26 09:15:50 +0000

    task_3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Python script to export data in the JSON format."""
+import json
+import requests
+
+
+def export_todo_data():
+    """function that exports data in the JSON format."""
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+    """Fetch user data."""
+    response_users = requests.get('{}/users'.format(BASE_URL))
+    users = response_users.json()
+
+    """Fetch TODO list data."""
+    response_todos = requests.get('{}/todos'.format(BASE_URL))
+    todos = response_todos.json()
+
+    """Create a dictionary to hold the unique JSON format data."""
+    todo_data = {}
+
+    for user in users:
+        user_id = user['id']
+        username = user['username']
+
+        """ Initialize the list for this user's tasks"""
+        user_tasks = []
+
+        for todo in todos:
+            if todo['userId'] == user_id:
+                task_data = {
+                    "username": username,
+                    "task": todo['title'],
+                    "completed": todo['completed']
+                }
+                user_tasks.append(task_data)
+
+        """ Add the user's tasks to the dictionary"""
+        todo_data[user_id] = user_tasks
+
+    """ Export the data to a unique JSON file """
+    unique_filename = 'todo_all_employees.json'
+    with open(unique_filename, 'w') as json_file:
+        json.dump(todo_data, json_file, indent=4)
+
+    print('TODO data for all employees exported to {}'.format(unique_filename))
+
+
+if __name__ == '__main__':
+    export_todo_data()
