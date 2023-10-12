955b0b07b25991e8556035e0d3d9d89fd7349fe8
Calebbkillom <Calebbkillom@gmail.com>
2023-10-12 22:10:31 +0000

    task-0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers """
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'YourCustomUserAgent/1.0'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data.get('data', {}).get('subscribers', 0)
+            return subscribers
+    except Exception:
+        pass
+
+    return 0
