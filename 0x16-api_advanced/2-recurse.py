2286e84ba672c2b2b9f7673d3af57a1fb9b711de
Calebbkillom <Calebbkillom@gmail.com>
2023-10-13 11:44:10 +0000

    task-2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,37 +1,35 @@
-#!usr/bin/python3
-""" function that queries the Reddit API and returns a list """
+#!/usr/bin/python3
+"""  recursive function that queries the Reddit API and returns a list """
 import requests
 
 
-def recurse(subreddit, hot_list=None, after=None):
-    """ function that queries the Reddit API and returns a list """
-    if hot_list is None:
-        hot_list = []
+def recurse(subreddit, after=None):
+    """
+    function that queries the Reddit API and returns a list
+    containing the titles of all hot articles for a given subreddit
+    """
+    if after is None:
+        after_param = ""
+    else:
+        after_param = "?after=" + after
 
-    base_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    user_agent = "UniqueRedditScraper/1.0"
-    headers = {'User-Agent': user_agent}
-    params = {'limit': 100, 'after': after}
+    url = "https://www.reddit.com/r/{}/hot.json{}"\
+        .format(subreddit, after_param)
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
-    response = requests.get(base_url, headers=headers,
-                            params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json().get('data', {})
-        articles = data.get('children', [])
-
-        for article in articles:
-            hot_list.append(article['data']['title'])
+        children = data.get('children', [])
+        hot_list = [child['data']['title'] for child in children]
 
         after = data.get('after')
+        if after:
+            hot_list += recurse(subreddit, after)
 
-        if not articles:
-            if not hot_list:
-                return None
-            else:
-                return hot_list
+        return hot_list
 
-        return recurse(subreddit, hot_list, after)
     elif response.status_code == 404:
         return None
     else:
